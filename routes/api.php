d62c32ee04747eb46aa14768d5920f487d5e3963
wokes <flyp.solutions@gmail.com>
2026-03-12 16:00:32 +0100

    Put POST /api/books behind Sanctum auth

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,9 @@ use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('books', BookController::class);
+// Required auth in accordance with the task description - it only mentioned this endpoint.
+Route::apiResource('books', BookController::class)->only(['store'])->middleware('auth:sanctum');
+
+// Public endpoints
+Route::apiResource('books', BookController::class)->except(['store']);
 Route::apiResource('authors', AuthorController::class)->only(['index', 'show']);
