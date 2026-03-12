2a89450d69fd0cf92b82d322561097f4c98c66c7
wokes <flyp.solutions@gmail.com>
2026-03-12 15:34:46 +0100

    Missed the route registration

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('books', BookController::class);
+Route::apiResource('authors', AuthorController::class)->only(['index', 'show']);
